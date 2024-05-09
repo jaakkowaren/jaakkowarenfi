f238f3f44e332ecde01c542272f16c7ce19f15fd
Jaakko Warén <jaakko.waren@gmail.com>
2024-05-09 13:30:35 +0300

    Quartz sync: May 9, 2024, 1:30 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -15,8 +15,9 @@ export default ((opts?: Options) => {
       <footer class={`${displayClass ?? ""}`}>
         <hr />
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz version on{version}</a> © {year}
+          AAAA
+          //{i18n(cfg.locale).components.footer.createdWith}{" "}
+          //<a href="https://quartz.jzhao.xyz/">Quartz version on{version}</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
